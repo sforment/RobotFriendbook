f74e5b7ed502aa41ce812bf2621d88274e864a6a
Stefan Formentano <StefanFormentano@gmail.com>
2018-12-02 13:57:32 -0600

    project at searchbox stage

---
+++ src/SearchBox.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SearchBox = () => {
+	return (
+		<div>
+			<input 
+				className='bg-lightest-blue pa3 b--light-blue'
+				type='search' 
+				placeholder='search robots'
+			/>
+		</div>
+	);
+}
+
+export default SearchBox;
